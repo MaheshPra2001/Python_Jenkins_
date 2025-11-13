bd4c628242b4a0664894b67b61edfaf6e124c76e
MaheshPrajapati <mp675089@gmail.com>
2025-11-13 23:12:16 +0530

    re-coded the testcase

--- test_app.py
+++ test_app.py
@@ -1,7 +1,14 @@
 import pytest
-from app import add_numbers
+from app import add_numbers,sub_numbers
 
 def test_add_numbers():
     assert add_numbers(2,3)==5
     assert add_numbers(-1,1)==0
     assert add_numbers(10,10)==20
+
+def test_sub_numbers():
+    assert sub_numbers(5,3)==2
+    assert sub_numbers(23,3)==20
+    assert sub_numbers(3,3)==0
+
+
